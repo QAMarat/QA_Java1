ac4f5002aff7d9a584f1dec671a5c395849fc81c
Marat <guysinmarat@gmail.com>
2020-04-07 16:33:27 +0100

    java weeks 1 files added

---
+++ src/comqac/Runner.java
@@ -0,0 +1,20 @@
+package comqac;
+
+public class Runner {
+	
+	public static void main(String args[]) {
+	
+	int age = 23; //variables
+	int height= 176;
+	String homeAdd= "123 Example Road London M50 3SA";
+	Boolean bool = true;
+	
+	
+	System.out.println("Hello World");
+	System.out.println("I am " + age);
+	System.out.println("I am " + height + " cm");
+	System.out.println(homeAdd);
+	System.out.println("The value of bool is" + bool);
+	}
+
+}
